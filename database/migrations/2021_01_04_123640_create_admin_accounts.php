b1fd5b1b6996a65650adcce50fbafa01ee6b39eb
daniel <danyel.maravillosa@gmail.com>
2021-01-04 20:40:53 +0800

    admin migrations

---
+++ database/migrations/2021_01_04_123640_create_admin_accounts.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'danyel.maravillosa@gmail.com',
+            'password' => '$2y$10$AXVvBVWXSIJkZDfsjHUnWeTGV5qzKNRelsn4Qchg5rhTUjVPtfgWm',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Principal',
+            'email' => 'principal@gmail.com',
+            'password' => '$2y$10$AXVvBVWXSIJkZDfsjHUnWeTGV5qzKNRelsn4Qchg5rhTUjVPtfgWm',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
