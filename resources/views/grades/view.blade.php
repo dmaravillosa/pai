912539d4579a49e905e43632cc1d5fb24092f1e6
daniel <danyel.maravillosa@gmail.com>
2021-01-31 02:27:15 +0800

    print button

--- resources/views/grades/view.blade.php
+++ resources/views/grades/view.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
 
-    <button type="button" class="btn btn-primary" onclick="printThis()">Print</button>
+        
         <div class="col-md-12 mt-2">
             <div class="row">
                 <div class="col-md-1 mt-2">
@@ -12,6 +12,9 @@
                 </div>
                 <div class="col-md-10 mt-2 text-center">
                     <h4>{{ $student->name }}</h4> <small>(Adviser: {{ $teacher }})</small>
+                </div>
+                <div class="col-md-1 mt-2">
+                    <button type="button" class="btn btn-primary" onclick="printThis()">Print</button>
                 </div>
             </div>
 
