d09cff30784407a295eb7d7fa7b5e508cd0f3e17
daniel <danyel.maravillosa@gmail.com>
2021-02-02 00:44:14 +0800

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,13 +25,14 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="First slide">
+        <!--<img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="First slide">-->
+        <img class="d-block w-100" src="{{ asset('home1.jpg') }}" alt="First slide">
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="Second slide">
+        <img class="d-block w-100" src="{{ asset('home2.jpg') }}" alt="Second slide">
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="Third slide">
+        <img class="d-block w-100" src="{{ asset('home3.jpg') }}" alt="Third slide">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
