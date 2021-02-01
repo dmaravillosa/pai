f91c1ec3efe36b7ec66a58973d797f2bc127eeee
daniel <danyel.maravillosa@gmail.com>
2021-02-02 00:21:13 +0800

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,35 @@
     </div>
 </div>
 
-<div class="col-md-10 ">
+<div class="col-md-10">
+<br><br>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="First slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="https://via.placeholder.com/600x200" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="col-md-12">
     <div class="row justify-content-center">
         @if(!isset($events[0]))
             <h5 class="mt-3">No current announcements.</h5>
@@ -34,5 +62,6 @@
         @endif
     </div>
 </div>  
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @endsection
